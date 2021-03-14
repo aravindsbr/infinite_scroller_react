5b45fa20e1742c1264590a93ec18252b4e9cc716
Aravinds <aravindsbr@gmail.com>
2021-03-14 08:55:39 +0530

    Removing unfavourites from favourites page

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -19,13 +19,14 @@ export default class Table extends React.Component {
             <div className="mt-2">
               <center>
                   { onlyFavourites  === "false" && 
-                    <h1><b>List of recent posts</b></h1>
+                    <div>
+                      <h1><b>List of recent posts</b></h1>
+                      <p><b>Showing {posts.length} posts from the URL</b></p>
+                    </div>
                   }
                   { onlyFavourites  === "true" && 
                     <h1><b>List of favourite posts</b></h1>
                   }
-                
-                <p><b>Showing {posts.length} posts from the URL</b></p>
                 <div className="mr-3 mb-3">
                   { onlyFavourites  === "false" && 
                     <Link to="/favourites" className="btn btn-danger fav_button">Favourites</Link>
@@ -56,6 +57,35 @@ export default class Table extends React.Component {
                   <tbody>
                     
                     {posts.map((post) => (
+                      onlyFavourites  === "false" &&
+                      <tr key={post.id}>
+                        <td>{post.userId}</td>
+                        <td>{post.id}</td>
+                        <td>{post.isFavourite.toString()}</td>
+                        <td>{post.title}</td>
+                        <td>{post.body}</td>
+                        <td>
+                        { post.isFavourite.toString()  === "true" && 
+                        <IconContext.Provider value={{ color: "red", size: "2em"}}>
+                          <div>
+                            <MdFavorite className="fav_icon" onClick={()=> onHandleFavourite(post)}/>
+                          </div>
+                        </IconContext.Provider>
+                        }
+                        { post.isFavourite.toString()  === "false" && 
+                        <IconContext.Provider value={{ size: "2em"}}>
+                          <div>
+                            <MdFavoriteBorder className="fav_icon" onClick={()=> onHandleFavourite(post)}/>
+                          </div>
+                        </IconContext.Provider>
+                        }
+                        </td>
+                      </tr>
+                      
+                    ))}
+
+                    {posts.map((post) => (
+                      onlyFavourites  === "true" && post.isFavourite.toString() === "true" && 
                       <tr key={post.id}>
                         <td>{post.userId}</td>
                         <td>{post.id}</td>
